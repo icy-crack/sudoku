0de6cfa10251ef28b658daf4cdf886129ebd9cdf
linxiaobing <linxiaobing@bytedance.com>
2019-01-15 13:39:46 +0800

    feat

--- src/data.js
+++ src/data.js
@@ -91,3 +91,57 @@ const data = [[
 ]];
 
 export default data;
+
+// let matrix = [];
+// // 随机生成第一行数
+// const allNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let tmpArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 8; i > 0; i--) {
+//     const random = Math.floor(Math.random() * (i + 1));
+//     [tmpArr[i], tmpArr[random]] = [tmpArr[random], tmpArr[i]];
+// }
+// matrix.push(tmpArr);
+// let cnt = 0;
+// while (matrix.length < 9) {
+//     cnt++;
+//     if (cnt > 10000 && matrix.length > 1) {
+//         cnt = 0;
+//         matrix.pop();
+//     }
+//     let arr = [];
+//     let curLine = matrix.length;
+//     matrix.push(arr);
+//     for (let i = 0; i < 9; i++) {
+//         let n = getRandomNum(curLine, i);
+//         if (!n) {
+//             // 此次随机选数失败，重新开始
+//             matrix.pop();
+//             break;
+//         }
+//         arr.push(n);
+//     }
+//     if (arr.length == 0) {
+//         matrix.pop();
+//     } else {
+//         cnt = 0;
+//     }
+// }
+// function getValidNum (line, col) {
+//     let excludeArr = [];
+//     for (let l = 0; l < line; l++) {
+//         excludeArr.push(matrix[l][col]);
+//     }
+//     for (let c = 0; c < col; c++) {
+//         excludeArr.push(matrix[line][c]);
+//     }
+//     return allNum.filter(item => {
+//         return !excludeArr.includes(item);
+//     })
+// }
+// function getRandomNum (line, col) {
+//     let validArr = getValidNum(line, col);
+//     if (validArr.length > 0) {
+//         return validArr[Math.floor(Math.random() * (validArr.length + 1))];
+//     }
+//     return 0;
+// }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,75 @@ import './util';
 import './style.css';
 
 (function () {
+    // init sudoku
     let app = document.querySelector('#app');
+    let cnt = 1;
     data.forEach((line, idx) => {
         let eleLine = document.createElement('div');
-        eleLine.addClass('line');
+        eleLine.classList.add('line');
         line.forEach(item => {
             let eleItem = document.createElement('div');
-            eleItem.addClass('item');
+            eleItem.classList.add('item');
             if (!item.hide) {
                 eleItem.innerText = item.value;
+                eleItem.setAttribute('data-value', item.value);
+                eleItem.classList.add('static');
+            } else {
+                eleItem.setAttribute('tabindex', cnt++);
             }
             eleLine.appendChild(eleItem);
         });
         app.appendChild(eleLine);
+    })
+})();
+
+(function () {
+    let eleNums = document.querySelector('.tool-nums');
+    // 锁定数字
+    eleNums.addEventListener('click', (event) => {
+        let target = event.target;
+        const findValue = target.innerText;
+        
+        let tipnumArr = document.querySelectorAll('.num');
+        let numArr = document.querySelectorAll('.item');
+
+        if (target.hasClass('active')) {
+            // 取消提示
+            target.classList.remove('active');
+            numArr.forEach(item => {
+                item.classList.remove('tips');
+            });
+            numArr.forEach(item => {
+                item.classList.remove('tips');
+            })
+        } else {
+            // 添加提示
+            tipnumArr.forEach(tip => {
+                tip.classList.remove('active');
+            });
+            target.classList.add('active');
+            if (findValue) {
+                numArr.forEach(item => {
+                    if (item.dataset.value == findValue) {
+                        item.classList.add('tips');
+                    } else {
+                        item.classList.remove('tips');
+                    }
+                })
+            }
+        }
+    });
+    // 填写
+    let eleItem = document.querySelectorAll('.item');
+    eleItem.forEach(item => {
+        item.addEventListener('focus', (event) => {
+            item.addEventListener('keydown', (e) => {
+                if (e.keyCode > 48 && e.keyCode < 58) {
+                    // 1 ~ 9
+                    item.innerText = e.key;
+                    item.setAttribute('data-value', e.key);
+                }
+            })  
+        });
     })
 })();
